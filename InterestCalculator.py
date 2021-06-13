34d643e2bb8a183a3a02258ca6a716c608f9f26c
jsgreen03 <jsgreen@calpoly.edu>
2021-06-13 16:19:42 -0700

    Init

---
+++ InterestCalculator.py
@@ -0,0 +1,20 @@
+
+def main():
+    print("Welcome to a sloppy script that calculates interest for you based on parameters you give!")
+    C = input("Please input your starting amount: ")
+    t = input("How long (in years) would you like interest to accrue for? ")
+    r = input("Please input the nominal interest rate: ")
+
+    return
+
+
+
+
+#Interest = C(1+(r/n))^(nt)
+#n = number of times compounded per time t
+#t = number of years in this case
+#r = interest rate
+#C = starting value
+
+if __name__ == "__main__":
+    main()
