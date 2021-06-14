395f8077a240ee3eb3732ce8c9d477d157e85eef
jsgreen03 <jsgreen@calpoly.edu>
2021-06-13 18:14:18 -0700

    V1.0

--- InterestCalculator.py
+++ InterestCalculator.py
@@ -1,12 +1,57 @@
 
 def main():
-    print("Welcome to a sloppy script that calculates interest for you based on parameters you give!")
-    C = input("Please input your starting amount: ")
-    t = input("How long (in years) would you like interest to accrue for? ")
-    r = input("Please input the nominal interest rate: ")
+    while True:
+        print("Welcome to a sloppy script that calculates interest for you based on parameters you give!")
+        c = getC()
+        t = getT()
+        r = getR()
+        comp = getComp()
+        output = calculate(c , t , r , comp)
+        print("Your resulting value is:\n" + str(output) + "\n")
 
-    return
 
+
+
+def getC():
+    c = input("Please input your starting amount:\n")
+    if not(str(c).isdigit()):
+        print("Don't try to break my fucking program")
+        getC()
+    return float(c)
+
+
+def getT():
+    t = input("How long (in years) would you like interest to accrue for?\n")
+    if not (str(t).isdigit()):
+        print("Don't try to break my fucking program")
+        getT()
+    return float(t)
+
+
+def getR():
+    r = input("Please input the nominal interest rate (Decimal):\n")
+    if not (str(r).isdigit()):
+        print("Don't try to break my fucking program")
+        getR()
+    return float(r)
+
+
+def getComp():
+    comp = input("How frequently would you like interest to compound? (Enter q for quarterly, a for annually, m for monthly)\n")
+    if comp not in "aqm":
+        print("Don't try to break my fucking program")
+        getComp()
+    return comp
+
+
+
+def calculate(c , t , r ,comp):
+    if comp == "q":
+        return c*(1+(r/4))**(4*t)
+    if comp == "a":
+        return c*(1+r)**t
+    if comp == "m":
+        return c*(1+(r/12))**(12*t)
 
 #comment
 
